58773675b2aedea56edce743c7ad8510c1d8fca8
saliha lammari <75095222+salihalammari@users.noreply.github.com>
2022-02-28 14:55:05 +0100

    project is finished

---
+++ so_long/bonus/ft_itoi_bonus.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoi_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slammari <slammari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 21:22:04 by slammari          #+#    #+#             */
+/*   Updated: 2022/02/27 18:44:27 by slammari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+static int	count_nbr(long long n)
+{
+	int	count;
+
+	count = 0;
+	if (n <= 0)
+		count++;
+	if (n < 0)
+		n = n * (-1);
+	while (n != 0)
+	{
+		count++;
+		n /= 10;
+	}
+	return (count);
+}
+
+static char	*calcul(long long nb, char *nbr, int len)
+{
+	int	sign;
+
+	sign = 0;
+	if (nb < 0)
+	{
+		sign = 1;
+		nb = nb * (-1);
+	}
+	else if (nb == 0)
+		nbr[0] = '0';
+	nbr[len] = '\0';
+	while (len--)
+	{
+		nbr[len] = nb % 10 + '0';
+		nb /= 10;
+	}
+	if (sign == 1)
+		nbr[0] = '-';
+	return (nbr);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*nbr;
+	long long	nb;
+	int			len;
+
+	nb = n;
+	len = count_nbr(nb);
+	nbr = malloc(len + 1 * sizeof(char));
+	if (!nbr)
+		return (NULL);
+	return (calcul(nb, nbr, len));
+}
+
+int	ft_strlen_nl(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (i);
+}
---
+++ so_long/get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slammari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 04:27:18 by slammari          #+#    #+#             */
+/*   Updated: 2022/02/22 18:31:29 by slammari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+
+#endif
